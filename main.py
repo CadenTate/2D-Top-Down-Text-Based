29b8f7a324b0a32091729ab5d6803922f053d60f
Caden Tate <cadentate0511@gmail.com>
2023-10-24 11:39:49 -0500

    Print World, Add Chunk, Move Player

---
+++ main.py
@@ -0,0 +1,45 @@
+from typing import Dict, List
+
+world = {}
+chunkLocation = [0,0]
+playerLocation = [5,5]
+currentTile = "X"
+
+def addChunk():
+    x = chunkLocation[0]
+    y = chunkLocation[1]
+    
+    world[x] = {}
+    world[x][y] = []
+    for i in range(81):
+        world[x][y].append("X")
+
+def printChunk():
+    chunk = world[chunkLocation[0]][chunkLocation[1]]
+    for i, tile in enumerate(chunk, 1):
+        if i % 9 != 0:
+            print(tile,end="")
+        else:
+            print(tile)
+
+def changePlayerLocation(chunkX:int,chunkY,playerX:int,playerY:int):
+    global playerLocation
+    # Verify within boundaries
+    if playerX >= 1 and playerX <= 9 and playerY >= 1 and playerY <= 9:
+        # Replace current position with tile
+        world[chunkLocation[0]][chunkLocation[1]][(playerLocation[0] - 1) * 9 + playerLocation[1] - 1] = currentTile
+        # Update current position to new position
+        playerLocation = [playerX,playerY]
+        # Get new current tile
+        currentTile = world[chunkLocation[0]][chunkLocation[1]][(playerLocation[0] - 1) * 9 + playerLocation[1] - 1]
+        # Put player at the new location
+        world[chunkLocation[0]][chunkLocation[1]][(playerLocation[0] - 1) * 9 + playerLocation[1] - 1] = "O"
+    else:
+        print("Invalid Position")
+        return -1
+
+addChunk()
+printChunk()
+changePlayerLocation(5,5)
+print("\n")
+printChunk()
