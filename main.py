3e933ef8c2cbeca15a7cdd74d7f99bde391b9ecc
CadenTate <cadentate0511@gmail.com>
2023-10-27 11:39:32 -0500

    Big Work

--- main.py
+++ main.py
@@ -2,44 +2,47 @@ from typing import Dict, List
 from generalFunctions import smartInput
 
 world = {}
-chunkLocation = [0,0]
-playerLocation = [5,5]
+chunkLocationX = 0
+chunkLocationY = 0
+playerLocationX = 5
+playerLocationY = 5
 currentTile = "X"
 
-def addChunk():
-    x = chunkLocation[0]
-    y = chunkLocation[1]
-    
-    world[x] = {}
-    world[x][y] = []
+def addChunk(newChunkX:int,newChunkY:int):
+    world[newChunkX] = {}
+    world[newChunkX][newChunkY] = []
     for i in range(81):
-        world[x][y].append("X")
+        world[newChunkX][newChunkY].append("X")
 
-def printChunk():
-    chunk = world[chunkLocation[0]][chunkLocation[1]]
+def printChunk(chunkX,chunkY):
+    chunk = world[chunkX][chunkY]
     for i, tile in enumerate(chunk, 1):
         if i % 9 != 0:
             print(tile,end="")
         else:
             print(tile)
 
-def changePlayerLocation(chunkX:int,chunkY,playerX:int,playerY:int):
-    global playerLocation
+def validatePosition(newChunkX:int,newChunkY,newPlayerX:int,newPlayerY:int):
+    if newChunkX != world or newChunkY != world:    
+        addChunk(newChunkX,newChunkY)
+
+
+def changePlayerLocation(newChunkX:int,newChunkY,newPlayerX:int,newPlayerY:int):
+    global playerLocationX, playerLocationY, currentTile
     # Verify within boundaries
-    if playerX >= 1 and playerX <= 9 and playerY >= 1 and playerY <= 9:
-        # Replace current position with tile
-        world[chunkLocation[0]][chunkLocation[1]][(playerLocation[0] - 1) * 9 + playerLocation[1] - 1] = currentTile
-        # Update current position to new position
-        playerLocation = [playerX,playerY]
-        # Get new current tile
-        currentTile = world[chunkLocation[0]][chunkLocation[1]][(playerLocation[0] - 1) * 9 + playerLocation[1] - 1]
-        # Put player at the new location
-        world[chunkLocation[0]][chunkLocation[1]][(playerLocation[0] - 1) * 9 + playerLocation[1] - 1] = "O"
-    else:
-        print("Invalid Position")
-        return -1
+    # Replace current position with tile
+    world[chunkLocationX][chunkLocationY][(playerLocationX - 1) * 9 + playerLocationY - 1] = currentTile
+    # Update current position to new position
+    playerLocation = [newPlayerX,newPlayerY]
+    # Get new current tile
+    currentTile = world[chunkLocationX][chunkLocationY][(playerLocationX - 1) * 9 + playerLocationY - 1]
+    # Put player at the new location
+    world[chunkLocationX][chunkLocationY][(playerLocationX - 1) * 9 + playerLocationY - 1] = "O"
 
 # Game Loop
 inPlay = True
 while inPlay:
-    userInput = smartInput("Enter Direction",validinput=("W","A","S","D"),case="upper")
+    userInput = smartInput("Enter Direction (W|A|S|D): ",validinput=("W","A","S","D"),case="upper")
+    match userInput:
+        case "W":
+            changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX,playerLocationY+1)
