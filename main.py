eb6c810dfe461015d0ae107da7b67e3dcbca6522
Caden Tate <cadentate0511@gmail.com>
2023-11-01 11:39:43 -0500

    Proper Moving Between Same Y Chunks

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ chunkLocationX = 0
 chunkLocationY = 0
 playerLocationX = 5
 playerLocationY = 5
-currentTile = "X"
+currentTile = "g"
 availableTiles = ["g","g","g","g","g","g","g","g","w","w"]
 
 def addChunk(newChunkX:int,newChunkY:int):
-    world[newChunkX] = {}
+    if newChunkX != None:
+        world[newChunkX] = {}
     world[newChunkX][newChunkY] = []
     for i in range(81):
         world[newChunkX][newChunkY].append(random.choice(availableTiles))
@@ -25,19 +26,29 @@ def printChunk(chunkX:int,chunkY:int):
             print(tile)
 
 def changePlayerLocation(newChunkX:int,newChunkY,newPlayerX:int,newPlayerY:int):
-    global playerLocationX, playerLocationY, currentTile
+    global playerLocationX, playerLocationY, currentTile, chunkLocationX, chunkLocationY
     # Verify within boundaries
-    if newChunkX not in world or newChunkY not in world[newChunkX]:
+    if newChunkX not in world:
         addChunk(newChunkX,newChunkY)
-    if newPlayerX >= 9:
-        newPlayerX = 0
-    if newPlayerY >= 9:
-        newPlayerY = 0
+    elif newChunkY not in world[newChunkX]:
+        addChunk(None,newChunkY)
+
+    if newPlayerX == 10:
+        newPlayerX = 1
+    elif newPlayerX == 0:
+        newPlayerX = 9
+
+    if newPlayerY == 10:
+        newPlayerY = 1
+    elif newPlayerY == 0:
+        newPlayerY = 9
     # Replace current position with tile
     world[chunkLocationX][chunkLocationY][(playerLocationY - 1)  * 9 + playerLocationX - 1] = currentTile
     # Update current position to new position
     playerLocationX = newPlayerX
     playerLocationY = newPlayerY
+    chunkLocationX = newChunkX
+    chunkLocationY = newChunkY
     # Get new current tile
     currentTile = world[chunkLocationX][chunkLocationY][(playerLocationY - 1) * 9 + playerLocationX - 1]
     # Put player at the new location
@@ -51,18 +62,29 @@ printChunk(chunkLocationX,chunkLocationY)
 
 # Game Loop
 while inPlay:
+    print(chunkLocationX,chunkLocationY)
+    print(playerLocationX,playerLocationY)
     userInput = smartInput("Enter Direction (W|A|S|D): ",validinput=("W","A","S","D"),case="upper")
     print(userInput)
     match userInput:
         case "W":
-            if playerLocationY - 1 <= 0: 
-                changePlayerLocation(chunkLocationX,chunkLocationY - 1,playerLocationX,playerLocationY-1)
+            if playerLocationY - 1 == 0: 
+                changePlayerLocation(chunkLocationX,chunkLocationY - 1,playerLocationX,playerLocationY - 1)
             else: 
                 changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX,playerLocationY-1)
         case "A":
-            changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX-1,playerLocationY)
+            if playerLocationX - 1 == 0:
+                changePlayerLocation(chunkLocationX-1,chunkLocationY,playerLocationX-1,playerLocationY)
+            else:
+                changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX-1,playerLocationY)
         case "S":
-            changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX,playerLocationY+1)
+            if playerLocationY + 1 == 10:
+                changePlayerLocation(chunkLocationX,chunkLocationY + 1,playerLocationX,playerLocationY+1)
+            else:
+                changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX,playerLocationY+1)
         case "D":
-            changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX+1,playerLocationY)
+            if playerLocationX + 1 == 10: 
+                changePlayerLocation(chunkLocationX + 1,chunkLocationY,playerLocationX+1,playerLocationY)
+            else:
+                changePlayerLocation(chunkLocationX,chunkLocationY,playerLocationX+1,playerLocationY)
     printChunk(chunkLocationX,chunkLocationY)
